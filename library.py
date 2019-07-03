27b7099217eb00666f208cac1c6d7faeeef024fd
szymon <sroziewski83>
2019-07-03 18:36:55 +0200

    broken rsi closes saving

--- library.py
+++ library.py
@@ -841,6 +841,7 @@ def relative_strength_index(_closes, n=14, _asset=None):
             _rsi[_i] = 100. - 100. / (1. + _rs)
     except Warning:
         logger_global[0].error("{} RSI computing error".format(_asset.market))
+        save_to_file(trades_logs_dir, "broken_rsi_closes_{}".format(time.time()), _closes)
         _rsi = [False]
 
     return _rsi
