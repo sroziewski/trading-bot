c7bd7919f9c5fc0b1d6e87b653670330d35023cd
szymon <sroziewski83>
2019-07-08 16:17:26 +0200

    print , STORJ

--- market_maker.py
+++ market_maker.py
@@ -13,13 +13,18 @@ def main():
 def price_buy_strategy():
     logger = setup_logger("price_buy_strategy")
     buy_assets = [
-        BuyAsset('CELR', 0.00000118, 0.00000101, 0.00000213, 100, 10)
+        BuyAsset('STORJ', 0.00001989, 0.00001908, 0.00004036, 100, 10)
     ]
     btc_value = get_remaining_btc()
     adjust_buy_asset_btc_volume(buy_assets, btc_value)
     check_buy_assets(buy_assets)
     logger.info("Starting price_buy_strategy :\n{}".format(
         '\n'.join(map(lambda _a: "{} :\t{}".format(_a.name, price_to_string(_a.price)), buy_assets))))
+    logger.info("Starting price_buy_strategy :\n{}".format(
+        '\n'.join(map(lambda _a: "{} buy :\t{} stop :\t{} profit :\t{}".format(_a.name, price_to_string(_a.buy_price),
+                                                                               price_to_string(_a.stop_loss_price),
+                                                                               price_to_string(_a.price_profit)),
+                      buy_assets))))
     observe_lower_price(buy_assets)
     logger.info("price_buy_strategy -- has finished")
     logger.info("Stop-loss and taking profits in progress only...")
@@ -32,7 +37,10 @@ def price_observer_strategy():
     ]
     check_observe_assets(buy_assets)
     logger.info("Starting price_observer_strategy :\n{}".format(
-        '\n'.join(map(lambda _a: "{} :\t{} :\t{} :\t{}".format(_a.name, price_to_string(_a.buy_price), price_to_string(_a.stop_loss_price), price_to_string(_a.price_profit)), buy_assets))))
+        '\n'.join(map(lambda _a: "{} buy :\t{} stop :\t{} profit :\t{}".format(_a.name, price_to_string(_a.buy_price),
+                                                                               price_to_string(_a.stop_loss_price),
+                                                                               price_to_string(_a.price_profit)),
+                      buy_assets))))
     for buy_asset in buy_assets:
         ObserverStrategy(buy_asset).run()
 
