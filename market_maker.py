a50957c4f97749c2d7f7609ea2c13d47fd1192a6
szymon <sroziewski83>
2019-07-08 16:43:57 +0200

    print , STORJ

--- market_maker.py
+++ market_maker.py
@@ -13,13 +13,13 @@ def main():
 def price_buy_strategy():
     logger = setup_logger("price_buy_strategy")
     buy_assets = [
-        BuyAsset('STORJ', 0.00001989, 0.00001908, 0.00004036, 100, 10)
+        BuyAsset('STORJ', 0.00001989, 0.00001900, 0.00004036, 100, 15)
     ]
     btc_value = get_remaining_btc()
     adjust_buy_asset_btc_volume(buy_assets, btc_value)
     check_buy_assets(buy_assets)
     logger.info("Starting price_buy_strategy :\n{}".format(
-        '\n'.join(map(lambda _a: "{} buy :\t{} stop :\t{} profit :\t{}".format(_a.name, price_to_string(_a.buy_price),
+        '\n'.join(map(lambda _a: "{} buy :\t{} stop :\t{} profit :\t{}".format(_a.name, price_to_string(_a.price),
                                                                                price_to_string(_a.stop_loss_price),
                                                                                price_to_string(_a.price_profit)),
                       buy_assets))))
@@ -31,7 +31,7 @@ def price_buy_strategy():
 def price_observer_strategy():
     logger = setup_logger("price_observer_strategy")
     buy_assets = [
-        ObserveAsset('DOGE', 0.00000039, 0.00000036, 0.00000097, 30)
+        ObserveAsset('STORJ', 0.00002137, 0.00002080, 0.00004036, 15)
     ]
     check_observe_assets(buy_assets)
     logger.info("Starting price_observer_strategy :\n{}".format(
