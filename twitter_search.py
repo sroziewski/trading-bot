7f2f3019617b18028c167c12e1765d20961aab13
szymon <sroziewski83>
2020-02-19 17:27:10 +0100

    twiter notofication

---
+++ twitter_search.py
@@ -0,0 +1,37 @@
+# import twitter
+#
+# api = twitter.Api()
+# statuses = api.GetUserTimeline(screen_name = "binance")
+# print([s.text for s in statuses])
+#
+# api = twitter.Api(consumer_key='twitter consumer key',
+#                   consumer_secret='twitter consumer secret',
+#                   access_token_key='the_key_given',
+#                   access_token_secret='the_key_secret')
+
+
+# import urllib.request
+# with urllib.request.urlopen('https://twitter.com/binance') as response:
+#    html = response.read()
+#    i = 1
+
+import shutil
+import tempfile
+import urllib.request
+
+# with urllib.request.urlopen() as response:
+#     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+#         shutil.copyfileobj(response, tmp_file)
+
+
+req = urllib.request.Request('https://twitter.com/binance')
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
+   if 'Scammers are impersonating official' in str(the_page):
+        i = 1
+
+# with open(tmp_file.name) as html:
+#     html.__str__()
+#     "Community Coin Vote Round"
+#     pass
+#
