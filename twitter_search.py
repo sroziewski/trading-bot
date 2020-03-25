58a13cba101dbcefda92b88dfe9c57b914428152
Szymon <sroziews@wp.pl>
2020-03-25 18:09:47 +0100

    twitter search, cycles

--- twitter_search.py
+++ twitter_search.py
@@ -10,14 +10,26 @@ url = 'https://twitter.com/binance'
 req = urllib.request.Request(url)
 
 pattern_s = re.compile('\\s+')
+cnt = 0
+t1 = 600
+cycle = 0
 
 while 1:
     with urllib.request.urlopen(req) as response:
         _page_content = response.read()
         _content = pattern_s.sub(" ", str(_page_content).lower())
         if 'community coin vote round' in _content:
+            cnt += 1
             send_mail("QQQ Community Coin Vote Round FOUND!!!", url)
             logger.info("Community Coin Vote Round -- found")
-            time.sleep(600)
+            time.sleep(t1)
+            if cnt > 3:
+                t1 *= 6
+                cnt = 0
+                cycle += 1
+            if cycle == 3:
+                cycle = 0
+                cnt = 0
+                t1 = 600
+                time.sleep(432000) # 5 days of sleeping, and we start from the beginning
         time.sleep(60)
-
