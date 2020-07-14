647475cfe5f0aaa5f551bbf1a93e51215276f5aa
szymon <fuckinshit12>
2020-07-14 18:06:26 +0200

    send mail at once // ref

--- market_setup_finder.py
+++ market_setup_finder.py
@@ -14,15 +14,9 @@ authorize()
 while 1:
     try:
         market_setups_binance = analyze_golden_cross("exclude-markets-binance", "1h", "1600 hours ago", "binance")
-        # if len(market_setups_binance) > 0:
-        #     send_mail("WWW Binance Setup Found WWW", ' '.join(format_found_markets(market_setups_binance)))
         _kucoin_ticker = "1hour"
         market_setups_kucoin = analyze_golden_cross("exclude-markets-kucoin", _kucoin_ticker,
                                                     get_kucoin_interval_unit(_kucoin_ticker, 1600), "kucoin")
-        # if len(market_setups_kucoin) > 0:
-        #     send_mail("WWW Kucoin Setup Found WWW", ' '.join(format_found_markets(market_setups_kucoin)))
-        #
-        # send_mail("WWW Binance Setup Found WWW", ' '.join(format_found_markets(market_setups_binance)))
         setup_tuples = [(market_setups_binance, "binance"), (market_setups_kucoin, "kucoin")]
         process_setups(setup_tuples)
         time.sleep(3500)
