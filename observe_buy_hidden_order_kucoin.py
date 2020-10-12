111d0ec5c9e86d6777519be999f88475bd5993a4
Szymon <sroziews@wp.pl>
2020-10-12 16:14:33 +0200

    sleeping

--- observe_buy_hidden_order_kucoin.py
+++ observe_buy_hidden_order_kucoin.py
@@ -12,9 +12,9 @@ def observe_kucoin_asset(_assets):
 side = KucoinClient.SIDE_BUY
 logger = setup_logger("kucoin-observe-hidden-order-{}".format(side))
 
-buy1 = BuyAsset("kucoin", "VIDT", 4173 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=1, stop_loss=False)
-buy2 = BuyAsset("kucoin", "CHR", 243 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=10, stop_loss=False)
-buy3 = BuyAsset("kucoin", "OCEAN", 2896 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=10, stop_loss=False)
+buy1 = BuyAsset("kucoin", "VIDT", 4173 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=180, stop_loss=False)
+buy2 = BuyAsset("kucoin", "CHR", 243 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=180, stop_loss=False)
+buy3 = BuyAsset("kucoin", "OCEAN", 2896 * sat, ratio=100, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, sleep=180, stop_loss=False)
 
 assets = [buy1, buy2, buy3]
 
