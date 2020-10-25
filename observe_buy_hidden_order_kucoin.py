2cdb40b874b09cff00c93d153153d5a0dcf91bb0
Simon <sroziewski@opi.org.pl>
2020-10-25 21:43:51 +0100

    observe with mailing on taking profits

--- observe_buy_hidden_order_kucoin.py
+++ observe_buy_hidden_order_kucoin.py
@@ -4,7 +4,9 @@ from binance.client import Client as BinanceClient
 from kucoin.client import Client as KucoinClient
 
 from library import BuyAsset, sat, setup_logger, watch_orders, \
-    set_kucoin_buy_orders, adjust_purchase_fund, wait_until_order_is_filled
+    set_kucoin_buy_orders, adjust_purchase_fund, wait_until_order_is_filled, authorize, send_mail, get_format_price
+
+authorize()
 
 side = KucoinClient.SIDE_BUY
 logger = setup_logger("kucoin-observe-hidden-order-{}".format(side))
@@ -24,6 +26,8 @@ set_kucoin_buy_orders(assets)
 bought_asset = watch_orders(assets)
 bought_asset.limit_hidden_order(is_profit=True)
 if wait_until_order_is_filled(bought_asset):
+    mail_content = f"XXX {bought_asset.market} Took Profit XXX", f"{bought_asset.size_profit} {bought_asset.name} @ {get_format_price(bought_asset.price_profit)} BTC : {get_format_price(bought_asset.adjusted_size * bought_asset.price_profit)} BTC"
+    send_mail(mail_content)
     sys.exit(0)
 
 
