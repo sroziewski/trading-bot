0b30b4dfbcd807746aa3f5fbf9d83e0d44d650c0
Simon <sroziewski@opi.org.pl>
2020-10-25 21:31:38 +0100

    fet

--- observe_buy_hidden_order_kucoin.py
+++ observe_buy_hidden_order_kucoin.py
@@ -13,7 +13,7 @@ buy1 = BuyAsset("kucoin", "OLT", 33.31 * sat, 29.3 * sat, 44.4 * sat, kucoin_sid
 buy2 = BuyAsset("kucoin", "CHR", 212 * sat, 191 * sat, 265 * sat, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, stop_loss=False)
 buy3 = BuyAsset("kucoin", "COTI", 253 * sat, 232 * sat, 298 * sat, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, stop_loss=False)
 buy4 = BuyAsset("kucoin", "CRO", 763 * sat, 711 * sat, 843 * sat, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, stop_loss=False)
-buy5 = BuyAsset("kucoin", "FET", 345 * sat, 310 * sat, 385 * sat, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, stop_loss=False)
+buy5 = BuyAsset("kucoin", "FET", 347 * sat, 310 * sat, 385 * sat, kucoin_side=side, ticker=BinanceClient.KLINE_INTERVAL_5MINUTE, stop_loss=False)
 
 
 assets = [buy1, buy2, buy3, buy4, buy5]
