1f5f0f4d75db36a6eef62a1b10d8ca10b567a51c
Simon <sroziewski@opi.org.pl>
2020-12-13 11:14:41 +0100

    printer

--- pattern_watcher_binance.py
+++ pattern_watcher_binance.py
@@ -82,5 +82,5 @@ while 1:
     if len(found_assets):
         mail_content = '<BR/>'.join(found_assets)
         send_mail(f"QQQ Binance Pattern Found QQQ", mail_content)
-        logger.info(' '.join(found_assets))
+        logger.info('\n'.join(found_assets))
     time.sleep(3600*4-60*5)
