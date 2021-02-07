e139d3065a55bedb9d7d47461af6b5a7d7ed8ddf
Simon <sroziewski@opi.org.pl>
2021-02-07 15:12:31 +0100

    watchasda

--- persist_candles.py
+++ persist_candles.py
@@ -272,10 +272,16 @@ def set_trade_volume(_schedule, _kline):
 
     _kline_timestamp = _kline.start_time
     if len(trades[_schedule.volume_crawl.market]) > 0:
-        _trades_list = list(filter(lambda x: 0 <= _kline_timestamp - x.timestamp <= _diff, trades[_schedule.volume_crawl.market]))
+        _trades_list0 = list(filter(lambda x: 0 <= x.timestamp - _kline_timestamp <= _diff, trades[_schedule.volume_crawl.market]))
+        _trades_list = []
         for _trade in trades[_schedule.volume_crawl.market]:
-            if 0 <= _kline_timestamp - _trade.timestamp <= _diff:
+            if 0 <= _trade.timestamp - _kline_timestamp <= _diff:
+                _trades_list.append(_trade)
                 logger.info(f"{_trade.market} trade: {_trade.timestamp_str} kline: {_kline_timestamp} trade: {_trade.timestamp} kline: {get_time_from_binance_tmstmp(_kline_timestamp)} diff: {_kline_timestamp-_trade.timestamp} {_trade.price} {_trade.quantity}")
+        logger.info("AAAAAAAAAAAAAA")
+        for _trade in _trades_list:
+            logger.info(
+                f"{_trade.market} trade: {_trade.timestamp_str} kline: {_kline_timestamp} trade: {_trade.timestamp} kline: {get_time_from_binance_tmstmp(_kline_timestamp)} diff: {_kline_timestamp - _trade.timestamp} {_trade.price} {_trade.quantity}")
         add_volumes(_trades_list, _kline)
 
 
