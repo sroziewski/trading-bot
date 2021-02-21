738bd43aa587d5ecfbdd46a8b05b864c806d4c2b
Simon <sroziewski@opi.org.pl>
2021-02-21 17:01:21 +0100

    hft

--- persist_candles.py
+++ persist_candles.py
@@ -243,10 +243,14 @@ def filter_current_trades(_vc):
 
 def _do_volume_crawl(_vc):
     trades[_vc.market] = []
+    last_tmstmp = datetime.datetime.now().timestamp()
     _bm = BinanceSocketManager(binance_obj.client)
     _conn_key = _bm.start_aggtrade_socket(_vc.market, process_trade_socket_message)
     _bm.start()
     while True:
+        _tmstmp_diff = datetime.datetime.now().timestamp() - last_tmstmp
+        if _tmstmp_diff > 60*5:
+            logger.warn(f"last trading volume tmstmp ({last_tmstmp}) is older than 5 minutes, diff = {_tmstmp_diff}")
         filter_current_trades(_vc)
         if len(trades[_vc.market]) > 0:
             logger.info(f"last trading volume : {trades[_vc.market][-1].timestamp_str} {_vc.market}")
@@ -306,6 +310,7 @@ def add_volumes(_trades_msgs, _kline):
 def process_trade_socket_message(_msg):
     _trade_msg = TradeMsg(_msg)
     trades[_trade_msg.market].append(_trade_msg)
+    last_tmstmp = datetime.datetime.now().timestamp()
 
 
 def manage_volume_crawling(_vc):
@@ -614,14 +619,14 @@ schedules.extend(get_binance_schedules("yfi"))
 schedules.extend(get_binance_schedules("dock"))
 schedules.extend(get_binance_schedules("perl"))
 
-# schedules.extend(get_kucoin_schedules("bepro"))
-# schedules.extend(get_kucoin_schedules("dag"))
-# schedules.extend(get_kucoin_schedules("vra"))
-# schedules.extend(get_kucoin_schedules("loki"))
-# schedules.extend(get_kucoin_schedules("tel"))
-# schedules.extend(get_kucoin_schedules("soul"))
-# schedules.extend(get_kucoin_schedules("ewt"))
-# schedules.extend(get_kucoin_schedules("nwc"))
-# schedules.extend(get_kucoin_schedules("cro"))
+schedules.extend(get_kucoin_schedules("bepro"))
+schedules.extend(get_kucoin_schedules("dag"))
+schedules.extend(get_kucoin_schedules("vra"))
+schedules.extend(get_kucoin_schedules("loki"))
+schedules.extend(get_kucoin_schedules("tel"))
+schedules.extend(get_kucoin_schedules("soul"))
+schedules.extend(get_kucoin_schedules("ewt"))
+schedules.extend(get_kucoin_schedules("nwc"))
+schedules.extend(get_kucoin_schedules("cro"))
 
 manage_crawling(schedules)
