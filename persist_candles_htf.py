20dd08da23f00b570d411ca14e843e696e0299f2
Simon <sroziewski@opi.org.pl>
2021-04-24 18:33:54 +0200

    adding ltf and htf files, finer order book

--- persist_candles_htf.py
+++ persist_candles_htf.py
@@ -262,7 +262,7 @@ def _do_volume_crawl(_vc):
     while True:
         _tmstmp_diff = datetime.datetime.now().timestamp() - last_tmstmp
         if _tmstmp_diff > 60*5:
-            logger.warn(f"last trading volume tmstmp ({last_tmstmp}) is older than 5 minutes, diff = {_tmstmp_diff}")
+            logger.warning(f"last trading volume tmstmp ({last_tmstmp}) is older than 5 minutes, diff = {_tmstmp_diff}")
         filter_current_trades(_vc)
         if len(trades[_vc.market]) > 0:
             logger.info(f"last trading volume : {trades[_vc.market][-1].timestamp_str} {_vc.market}")
