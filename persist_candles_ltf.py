2156431b3f96a887c2897ca0193f365265ae51cf
Simon <sroziewski@opi.org.pl>
2021-07-02 00:38:27 +0200

    gold added front removed from usdt market

--- persist_candles_ltf.py
+++ persist_candles_ltf.py
@@ -699,6 +699,7 @@ schedules.extend(get_binance_schedules("uni"))
 schedules.extend(get_binance_schedules("skl"))
 schedules.extend(get_binance_schedules("near"))
 schedules.extend(get_binance_schedules("storj"))
+schedules.extend(get_binance_schedules("paxg"))
 
 # schedules.extend(get_kucoin_schedules("bepro"))
 # schedules.extend(get_kucoin_schedules("dag"))
