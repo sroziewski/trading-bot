979d2a0f447b1598db8f846cb8671dfe0036376d
Simon <sroziewski@opi.org.pl>
2022-10-25 15:05:58 +0200

    volume scan

---
+++ importdb.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import more_itertools as mit
+
+files = os.listdir('/mongo-backup/cont')
+coins = list(map(lambda y: y.split(".")[0], filter(lambda x: 'bson' in x, files)))
+
+data_in = list(mit.chunked(coins, 4))
+
+for chunk in data_in:
+    for el in chunk:
+        cmd = "mv  cont/{}.bson cont/{}.metadata.json klines".format(el, el)
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        process = subprocess.Popen("mongorestore klines", shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        process = subprocess.Popen("rm klines/*", shell=True, stdout=subprocess.PIPE)
+        process.wait()
