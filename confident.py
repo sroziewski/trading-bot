51fcc30c09233227d885bdfc793610116be23c72
Simon <sroziewski@opi.org.pl>
2023-03-02 16:57:44 +0100

    pix

--- confident.py
+++ confident.py
@@ -19,6 +19,7 @@ counter = 0
 
 
 def read_text(_file_fullname, _filename):
+    global locked
     im = Image.open(r""+_file_fullname)
     left = 5
     top = 5
@@ -27,7 +28,11 @@ def read_text(_file_fullname, _filename):
     img_crop = im.crop((left, top, right, bottom))
     _tmp_file = path + "tmp/" + _filename
     img_crop.save(_tmp_file)
+    while locked:
+        sleep(1)
+    locked = True
     tesseract_cmd = "docker run --rm -it --name myapp -v \"/var/www/html/pics/tmp\":/app  -w /app \"tesseract-ocr\" tesseract {} stdout --oem 1".format(_filename)
+    locked = False
     p = subprocess.Popen(tesseract_cmd, stdout=subprocess.PIPE, shell=True)
     return p.communicate()
 
@@ -38,10 +43,10 @@ class Argument(object):
         self.when = _when
 
 
-_list = []
+locked = False
+
 
 def save_pic(_arg: Argument):
-    global _list
     _date = date.today()
     if _arg.when == "yesterday":
         _date -= datetime.timedelta(1)
@@ -54,14 +59,12 @@ def save_pic(_arg: Argument):
         for _sec in range(60):
             _url_tmp = prefix_url + "{}/{}/Screen-Shot-{}-{}-{}-at-7.{}.{}-AM.png" \
                 .format(_year, _month, _year, _month, _day, add_zero(_min), add_zero(_sec))
-            _list.append(_url_tmp)
             try:
                 _filename_suffix = "{}-{}.png".format(_arg.when, _counter)
                 _img_filename = path + _filename_suffix
                 _img_filename_small = path + "small/{}-{}.png".format(_arg.when, _counter)
                 request.urlretrieve(_url_tmp, _img_filename)
                 resize_pic(_img_filename, _img_filename_small)
-
                 _txt = read_text(_img_filename, _filename_suffix)
                 print(_txt)
                 _counter += 1
@@ -107,7 +110,7 @@ def manager():
         _f.write(str(datetime.datetime.now()))
 
 
-schedule.every().day.at("16:49").do(manager)
+schedule.every().day.at("16:59").do(manager)
 schedule.every().day.at("20:30").do(manager)
 #
 while True:
