3f4dbca9be658bc1ee76f2b7ef4e9ff0b8af9db4
Simon <sroziewski@opi.org.pl>
2023-05-12 20:48:00 +0200

    new volume levels added

---
+++ retrieve_all_history.py
@@ -0,0 +1,56 @@
+import threading
+from time import sleep
+
+from bson import CodecOptions
+from bson.codec_options import TypeRegistry
+
+from library import DecimalCodec, lib_initialize, try_get_klines, save_to_file
+from mongodb import mongo_client
+
+lib_initialize()
+
+
+class Retrieve(object):
+    def __init__(self, _market, _ticker):
+        self.market = _market
+        self.ticker = _ticker
+
+
+threads = {}
+
+
+def _do_retrieve(_retrieve: Retrieve):
+    klines = try_get_klines("binance", _retrieve.market.upper(), _retrieve.ticker, "3000 days ago")
+    _filename = "{}_{}".format(_retrieve.market, _retrieve.ticker)
+    save_to_file("E:/data/binance/klines/", _filename, klines)
+    del threads["{}{}".format(_retrieve.market, _retrieve.ticker)]
+
+
+def manage_retrieve_scan(_r: Retrieve):
+    _crawler = threading.Thread(target=_do_retrieve, args=(_r,),
+                                name='_do_retrieve : {}'.format(_r.market))
+    _crawler.start()
+    threads["{}{}".format(_r.market, _r.ticker)] = 1
+
+
+db_markets_info = mongo_client.markets_info
+db_journal = mongo_client.journal
+
+decimal_codec = DecimalCodec()
+type_registry = TypeRegistry([decimal_codec])
+codec_options = CodecOptions(type_registry=type_registry)
+
+btc_markets_collection = db_markets_info.get_collection("btc", codec_options=codec_options)
+usdt_markets_collection = db_markets_info.get_collection("usdt", codec_options=codec_options)
+
+_market_info_cursor = usdt_markets_collection.find()
+_market_info_list = [e for e in _market_info_cursor]
+
+
+for _market_s in _market_info_list:
+    for _ticker in _market_s['tickers']:
+        if _ticker not in ['2d', '4d', '5d']:
+            _market = "{}{}".format(_market_s['name'], "usdt")
+            manage_retrieve_scan(Retrieve(_market, _ticker))
+            while len(threads.values()) > 5:
+                sleep(1)
