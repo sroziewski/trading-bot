75905000950ba476024658d4ee687a95ad2ac344
Simon <sroziewski@opi.org.pl>
2023-06-26 19:24:45 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -2,6 +2,35 @@
 
 $version = file_exists("pics/date.txt") ? file_get_contents("pics/date.txt") : "Empty";
 
-echo "<h1>$version</h1>"
+if($_SESSION['user_name']!="simon"){
+    header("Location: index.php");
+    exit();
+}
 
 ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>LOGIN</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+        <h2><?php echo($version); ?></h2>
+
+     <form action="login.php" method="post">
+
+
+
+
+     </form>
+
+</body>
+
+</html>
