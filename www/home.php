d5fb0d2b1a7a9d210caae09f0f9b95b531033041
Simon <sroziewski@opi.org.pl>
2023-06-26 21:16:05 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -32,7 +32,7 @@ if($_SESSION['user_name']!="simon"){
             echo('<center>'.$val.'</br><table>');
             $i = 0;
             foreach($arr as $row){
-                echo('<tr><td class="nobackground">'.++$i.'. <a href="pics/'.$row.'"><img src="pics/small/'.$row.'"/></a></td></tr>');
+                echo('<tr><td class="nobackground">'.++$i.'.&nbsp;<a href="pics/'.$row.'"><img src="pics/small/'.$row.'"/></a></td></tr>');
             }
             echo('</table></center>');
     }
