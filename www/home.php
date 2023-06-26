589df32864e8a44b6a0458343f0bff242c49e4e2
Simon <sroziewski@opi.org.pl>
2023-06-26 20:24:53 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -23,9 +23,48 @@ if($_SESSION['user_name']!="simon"){
 
 <body>
         <h2><?php echo($version); ?></h2>
-<?php
-        print_r($_GET);
-        echo("pics/map/txt");
+    <?php
+    if(!isset($_GET['name'])){
+        echo('<table>
+        <tr><td><a href="?name=btc">BTC</a></td>
+            <td><a href="?name=eth">ETH</a></td>
+            <td><a href="?name=ada">ADA</a></td>
+            <td><a href="?name=ape">APE</a></td>
+        </tr>
+        <tr><td><a href="?name=sand">SAND</a></td>
+            <td><a href="?name=sol">SOL</a></td>
+            <td><a href="?name=dot">DOT</a></td>
+            <td><a href="?name=avax">AVAX</a></td>
+        </tr>
+        <tr><td><a href="?name=doge">DOGE</a></td>
+            <td><a href="?name=link">LINK</a></td>
+            <td><a href="?name=shiba">SHIBA</a></td>
+            <td><a href="?name=mana">MANA</a></td>
+        </tr>
+        <tr><td><a href="?name=algo">ALGO</a></td>
+            <td><a href="?name=atom">ATOM</a></td>
+            <td><a href="?name=matic">MATIC</a></td>
+            <td><a href="?name=LTC">ltc</a></td>
+        </tr>
+        <tr><td><a href="?name=eos">EOS</a></td>
+            <td><a href="?name=uni">UNI</a></td>
+            <td><a href="?name=vet">VET</a></td>
+            <td><a href="?name=xrp">XRP</a></td>
+        </tr>
+        <tr><td colspan="4"><a href="?name=others">OTHERS</a></td>
+        </tr>
+        </table>');
+        }
+        else{
+            echo('<center>');
+            echo("pics/map/".$_GET['name'].".txt");
+            foreach (new SplFileObject("pics/map/".$_GET['name']+".txt") as $fname) {
+                echo('<img src="pics/small/'.$fname+'"/>');
+                echo('pics/small/'.$fname+'</br>');
+            }
+            echo('</center>');
+        }
+        echo(">"+$_GET['name']+"<");
 ?>
 </body>
 
