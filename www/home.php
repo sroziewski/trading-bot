2aa1f7b382aa8374a64bf6a6aa2aa808f1d37881
Simon <sroziewski@opi.org.pl>
2023-06-26 19:48:44 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -30,6 +30,28 @@ if($_SESSION['user_name']!="simon"){
             <td><a href="?name=ada">ADA</a></td>
             <td><a href="?name=ape">APE</a></td>
         </tr>
+        <tr><td><a href="?name=sand">SAND</a></td>
+            <td><a href="?name=sol">SOL</a></td>
+            <td><a href="?name=dot">DOT</a></td>
+            <td><a href="?name=avax">AVAX</a></td>
+        </tr>
+        <tr><td><a href="?name=doge">DOGE</a></td>
+            <td><a href="?name=link">LINK</a></td>
+            <td><a href="?name=shiba">SHIBA</a></td>
+            <td><a href="?name=mana">MANA</a></td>
+        </tr>
+        <tr><td><a href="?name=algo">ALGO</a></td>
+            <td><a href="?name=atom">ATOM</a></td>
+            <td><a href="?name=matic">MATIC</a></td>
+            <td><a href="?name=LTC">ltc</a></td>
+        </tr>
+        <tr><td><a href="?name=eos">EOS</a></td>
+            <td><a href="?name=uni">UNI</a></td>
+            <td><a href="?name=vet">VET</a></td>
+            <td><a href="?name=xrp">XRP</a></td>
+        </tr>
+        <tr><td colspan="4"><a href="?name=others">OTHERS</a></td>
+        </tr>
         </table>
 
 
