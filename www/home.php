ab4292f4b6a62cd7f76c8890202bd6e5ea6cc8cc
Simon <sroziewski@opi.org.pl>
2023-06-26 20:11:41 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -15,7 +15,7 @@ if($_SESSION['user_name']!="simon"){
 
 <head>
 
-    <title>LOGIN</title>
+    <title>Home</title>
 
     <link rel="stylesheet" type="text/css" href="style.css">
 
@@ -58,6 +58,7 @@ if($_SESSION['user_name']!="simon"){
         }
         else{
             echo('<center>');
+            echo("pics/map/"+$_GET['name']+".txt");
             foreach (new SplFileObject("pics/map/"+$_GET['name']+".txt") as $fname) {
                 echo('<img src="pics/small/'+$fname+'"/>');
                 echo('pics/small/'+$fname+'</br>');
