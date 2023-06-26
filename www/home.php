1c2edc213468653881180820a9d7f85a2329d49e
Simon <sroziewski@opi.org.pl>
2023-06-26 21:04:13 +0200

    asd

--- www/home.php
+++ www/home.php
@@ -73,9 +73,9 @@ if($_SESSION['user_name']!="simon"){
             $today = array();
             $yesterday = array();
             foreach (new SplFileObject("pics/map/".$_GET['name'].".txt") as $fname) {
-//                 if(str_contains($fname, "today"){
-//                     array_push($today, $fname);
-//                 }
+                if(str_contains($fname, "today"){
+                    echo($fname);
+                }
 //                 if(str_contains($fname, "yesterday"){
 //                     array_push($yesterday, $fname);
 //                 }
