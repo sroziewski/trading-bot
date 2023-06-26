15133eb3358a9cc634293edadc72b78e9483ba51
Simon <sroziewski@opi.org.pl>
2023-06-26 18:22:02 +0200

    asd

--- confident.py
+++ confident.py
@@ -240,7 +240,7 @@ def clear_chart_dir():
 
 schedule.every().day.at("07:58").do(clear_chart_dir)
 schedule.every().day.at("08:00").do(manager)
-schedule.every().day.at("18:12").do(manager)
+schedule.every().day.at("18:23").do(manager)
 #
 while True:
     # Checks whether a scheduled task
