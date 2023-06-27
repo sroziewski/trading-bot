68c46d914b25fcca539a7279c14dfd926b7eedad
Simon <sroziewski@opi.org.pl>
2023-06-27 11:24:34 +0200

    path cc

--- rename.py
+++ rename.py
@@ -3,8 +3,8 @@ import uuid
 
 from library import get_files
 
-path = "/var/www/html/pics/small/"
-destiny = "/var/www/html/pics/small/store/"
+path = "/var/www/html/pics/small"
+destiny = "{}/store/".format(path)
 
 for file in get_files(path):
     shutil.move(path+file, destiny+uuid.uuid4().__str__()+".png")
