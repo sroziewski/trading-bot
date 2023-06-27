f748b0dbb58e4abc1774628aeba39a01a0b8fca2
Simon <sroziewski@opi.org.pl>
2023-06-27 11:15:29 +0200

    cc

--- www/login.php
+++ www/login.php
@@ -2,59 +2,33 @@
 
 session_start();
 
-if (isset($_POST['uname']) && isset($_POST['password'])) {
+function validate($data){
 
-    function validate($data){
+   $data = trim($data);
 
-       $data = trim($data);
+   $data = stripslashes($data);
 
-       $data = stripslashes($data);
+   $data = htmlspecialchars($data);
 
-       $data = htmlspecialchars($data);
+   return $data;
 
-       return $data;
+}
 
-    }
+$uname = validate($_POST['uname']);
 
-    $uname = validate($_POST['uname']);
+$pass = validate($_POST['password']);
 
-    $pass = validate($_POST['password']);
+ if ("b30bd351371c686298d32281b337e8e9" === md5($uname) && "a68d43acea73feb7065e2cee36916e5e" === md5($pass)) {
 
-    if (empty($uname)) {
+    echo "Logged in!";
 
-        header("Location: index.php");
+    $_SESSION['user_name'] = $uname;
 
-        exit();
+    header("Location: home.php");
 
-    }else if(empty($pass)){
+    exit();
 
-        header("Location: index.php");
-
-        exit();
-
-    }else{
-
-            if ("b30bd351371c686298d32281b337e8e9" === md5($uname) && "a68d43acea73feb7065e2cee36916e5e" === md5($pass)) {
-
-                echo "Logged in!";
-
-                $_SESSION['user_name'] = $uname;
-
-                header("Location: home.php");
-
-                exit();
-
-            }else {
-
-                header("Location: index.php");
-
-                exit();
-
-            }
-
-        }
-
-}else{
+ }else {
 
     header("Location: index.php");
 
