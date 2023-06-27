e2d12de5168b60b0a7c40b9a95b872c2854a6d2d
Simon <sroziewski@opi.org.pl>
2023-06-27 10:46:16 +0200

    md5

--- www/login.php
+++ www/login.php
@@ -34,7 +34,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
     }else{
 
-            if ("simon" === $uname && "a68d43acea73feb7065e2cee36916e5e" === md5($pass)) {
+            if ("b30bd351371c686298d32281b337e8e9" === md5($uname) && "a68d43acea73feb7065e2cee36916e5e" === md5($pass)) {
 
                 echo "Logged in!";
 
